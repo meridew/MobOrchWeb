f6b53e823ca040e8c1cfa63f4e3c64fd2928289a
meridew <63099518+meridew@users.noreply.github.com>
2023-09-18 08:54:23 +0100

    standardised naming, improved nav and content

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,15 +1,19 @@
 // Load content into a placeholder
-function loadComponent(placeholderId, componentUrl) {
+function loadComponent(placeholderId, componentUrl, callback) {
     fetch(componentUrl)
         .then(response => response.text())
         .then(content => {
             const placeholder = document.getElementById(placeholderId);
             if (placeholder) {
                 placeholder.innerHTML = content;
+                if (callback) {
+                    callback();
+                }
             }
         })
         .catch(error => console.error('Error loading component:', error));
 }
+
 
 // Remove content from a placeholder
 function removeComponent(placeholderId) {
